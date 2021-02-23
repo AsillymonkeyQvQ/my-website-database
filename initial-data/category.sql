201575b174f40722a297ba866d29dae3708918df
sunzehai <zehai.sun@icloud.com>
2021-02-23 16:27:58 +0900

    添加子分类

--- initial-data/category.sql
+++ initial-data/category.sql
@@ -2,3 +2,4 @@ INSERT INTO category(id, parentId, name, description) VALUES(1, 0, 'LIFESTYLE', 
 INSERT INTO category(id, parentId, name, description) VALUES(2, 0, 'TECHNOLOGY', '');
 INSERT INTO category(id, parentId, name, description) VALUES(3, 0, 'JAPANESE', '');
 INSERT INTO category(id, parentId, name, description) VALUES(4, 0, 'ANIMATION', '');
+INSERT INTO category(id, parentId, name, description) VALUES(5, 2, 'DESIGN PATTERN', '');
